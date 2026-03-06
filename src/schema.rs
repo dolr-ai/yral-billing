990285eba70dd6016b5d4e2cbb373a806d2afd24
Ravi Sawlani <ravisawlani04@gmail.com>
2026-03-06 21:09:09 +0530

    added tests

--- src/schema.rs
+++ src/schema.rs
@@ -1,17 +1,6 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    purchase_tokens (id) {
-        id -> Text,
-        user_id -> Text,
-        purchase_token -> Text,
-        status -> Text,
-        created_at -> Timestamp,
-        expiry_at -> Timestamp,
-    }
-}
-
-diesel::table! {
     bot_chat_access (id) {
         id -> Text,
         purchase_token -> Text,
@@ -23,3 +12,16 @@ diesel::table! {
         expires_at -> Timestamp,
     }
 }
+
+diesel::table! {
+    purchase_tokens (id) {
+        id -> Text,
+        user_id -> Text,
+        purchase_token -> Text,
+        status -> Text,
+        created_at -> Timestamp,
+        expiry_at -> Timestamp,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(bot_chat_access, purchase_tokens,);
